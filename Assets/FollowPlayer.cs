9ae115263677c68904c45f356a14afd9bbe9636e
DESKTOP-O1U6U1E\prutm <prutmaild@gmail.com>
2020-06-27 18:02:34 +0200

    First commit added player controller (WIP)

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform cameraTarget;
+    [SerializeField] private Transform player;
+
+    [SerializeField] private int rotationSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = cameraTarget.position;
+
+        Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed);
+    }
+}
