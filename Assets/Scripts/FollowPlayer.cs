43112adb2dbde7106bedf2c76440ca3ed6fd7fa9
DESKTOP-O1U6U1E\prutm <prutmaild@gmail.com>
2020-06-28 22:59:04 +0200

    Changed from euler to quaternions for much better flight control

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] PlayerController playerController;
+    [SerializeField] Vector3 playerOffset = new Vector3(0, 8, -15);
+    [SerializeField] private float transitionSpeed = 6;
+
+    void Update()
+    {
+        Vector3 localOffset = playerController.transform.right * playerOffset.x + playerController.transform.up * playerOffset.y + playerController.transform.forward * playerOffset.z;
+
+        Vector3 desiredPosition = playerController.transform.position + localOffset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * transitionSpeed);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, playerController.transform.rotation, Time.deltaTime * transitionSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float leftRightSpeed = 60;
+    [SerializeField] float upDownSpeed = 80;
+    [SerializeField] float speed = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        CheckMovement();
+    }
+
+    public void CheckMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (verticalInput != 0)
+        {
+            MoveUpDown(verticalInput);
+        }
+
+        if (horizontalInput != 0)
+        {
+            MoveLeftRight(-horizontalInput);
+        }
+    }
+
+    public void MoveUpDown(float addValue)
+    {
+        addValue *= Time.deltaTime * upDownSpeed;
+
+        Quaternion rotator = Quaternion.AngleAxis(addValue, transform.right);
+        transform.rotation = rotator * transform.rotation;
+    }
+
+    public void MoveLeftRight(float addValue)
+    {
+        addValue *= Time.deltaTime * leftRightSpeed;
+
+        Quaternion rotator = Quaternion.AngleAxis(addValue, transform.forward);
+        transform.rotation = rotator * transform.rotation;
+    }
+}
